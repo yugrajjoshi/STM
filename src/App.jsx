bfd761e0d537e8fa4e4993c22a4a039e437d2ed4
Yugraj <yugrajjoshi185@gmail.com>
2026-02-13 19:51:41 +0530

    Add smartTaskManager project files and fix gitignore

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+import './App.css'
+
+function App() {
+  
+     
+  return (
+    <>
+     <header>
+      <div className="app-container">
+        <h1 className="banner">Smart Task Manager</h1>
+      </div>
+     </header>
+    </>
+  )
+}
+export default App
---
+++ src/components/newtaskcreater.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function createTask(){
+    return(
+        <>
+          <div>
+          </div>
+        </>
+        
+    )
+}
+
+
+
+
+
+
+export default createTask
